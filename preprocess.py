83782fd531b63031052fb5de40f9402221a01da7
xexxw <90753521+xexxw@users.noreply.github.com>
2023-08-18 13:42:04 +0800

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -1,5 +1,5 @@
 import jieba
-
+import re
 
 
 def preprocess_text(content_lines, sentences, category):
@@ -8,7 +8,8 @@ def preprocess_text(content_lines, sentences, category):
             line = line.lower()
             line = line.replace('[br]', '')
             segs = jieba.lcut(line)
-            
+            # 使用正则表达式过滤标点符号
+            line = re.sub(r'[^\w\s]', '', line)
             
             # 过滤长度小于1的词 
             segs = list(filter(lambda x:len(x)>1, segs))
