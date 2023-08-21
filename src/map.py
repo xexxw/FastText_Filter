5c1768f869907b01b17144a861015a4eb1397e10
xexxw <90753521+xexxw@users.noreply.github.com>
2023-08-21 15:45:41 +0800

    Create map.py

---
+++ src/map.py
@@ -0,0 +1,44 @@
+import sys
+import fasttext
+import json
+import jieba
+import re
+from comm import sys_stderr_write
+
+
+
+# load model
+model = fasttext.load_model('text_cls_fasttext.bin')
+
+dict_cate = {'__label__1':'1', '__label__2':'2'}
+
+# read content
+for line in sys.stdin:
+    line = line.strip()
+    # 按制表符分割行
+    columns = line.split('\t')
+    # 输出第二列
+    # 把json字符串转换成python对象
+    data = json.loads(columns[1])
+    content = data["content"]
+
+    
+    # preprocess data    
+    content = content.lower()
+    content = content.replace('[br]', '')
+    # # 使用正则表达式过滤标点符号
+    # content = re.sub(r'[^\w\s]', '', content)
+    seg = jieba.lcut(content)
+    content = " ".join(segs)
+    
+    
+    # predict
+    pred_label = model.predict(content)
+    pred_res = str(dict_cate[pred_label[0][0]])
+
+    
+    # output
+    if pred_res == '1':
+        print('\t'.join(columns)+"#1")
+    else:
+        print('\t'.join(columns)+"#2")
